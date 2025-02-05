3f90cff0313813539f3f743963cfa5d5226859d3
MohamedHesham2024 <mohmedhesham2024@gmail.com>
2025-02-05 19:40:41 +0200

    FIX:solve body-parser

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { AppModule } from './app.module';
 import { SwaggerConfig } from './config/swagger';
 import { RabbitMqConfigModule } from './config/rabbitmq-config.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
